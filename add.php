4592a7469618feb1bc737827b799801d93886796
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2021-01-03 11:50:54 -0800

    form made

--- add.php
+++ add.php
@@ -1,47 +1,49 @@
 <?php 
+  $title = $email = $ingredients;
+  $errors = array('email'=>'', 'title'=>'', 'ingredients'=>'');
 
   if(isset($_POST['submit'])){
-    echo htmlspecialchars($_POST['email <br />']);
-    echo htmlspecialchars($_POST['title <br />']);
-    echo htmlspecialchars($_POST['ingredients <br />']);
-  }
-
-  if(empty($_POST['email'])){
-    echo 'An email is required <br />';
-  } else {
-    $email = $_POST['email'];
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-      echo 'email must be valid';
+    // echo htmlspecialchars($_POST['email <br />']);
+    // echo htmlspecialchars($_POST['title <br />']);
+    // echo htmlspecialchars($_POST['ingredients <br />']);
+    
+    if(empty($_POST['email'])){
+      $errors['email']='An email is required <br />';
+    } else {
+      $email = $_POST['email'];
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['email']= 'email must be valid';
+      }
     }
-  }
-
-  if(empty($_POST['title'])){
-    echo 'title required <br />';
-  } else {
-    $title = $_POST['title'];
-    if(!preg_match('/^[a-zA-Z\s]+$', $title)){
-      echo 'Title must be letters and space only  <br />';
+    
+    if(empty($_POST['title'])){
+      $errors['title'] =  'title required <br />';
+    } else {
+      $title = $_POST['title'];
+      if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+        $errors['title'] = 'Title must be letters and space only  ';
+      }
     }
+    
+    if(empty($_POST['ingredients'])){
+      $errors['ingredients'] = 'ingredients required <br />';
+    } 
+    else {
+      $ingredients = $_POST['ingredients'];
+      if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+          $error['ingredients'] = 'ingredients must be letters and space only';
+        }
+      }
+      
+      // globals are $_
+      // if(isset($_GET['submit'])){
+        //   echo $_GET['email'];
+        //   echo $_GET['title'];
+        //   echo $_GET['ingredients'];
+        // }
+        // // globals are $_
+        
   }
-
-  if(empty($_POST['ingredients'])){
-    echo 'ingredients required <br />';
-  } else {
-    // $ingredients = $_POST['ingredients'];
-    // if(!preg_match('/^[a-zA-Z\s]+$', $ingredients)){
-    //   echo 'ingredients must be letters and space only  <br />';
-    // }
-  }
-
-
-  // globals are $_
-  // if(isset($_GET['submit'])){
-  //   echo $_GET['email'];
-  //   echo $_GET['title'];
-  //   echo $_GET['ingredients'];
-  // }
-  // // globals are $_
-
 ?>
 
 <!DOCTYPE html>
@@ -53,11 +55,14 @@
       <h4 class="center">Add a Pizza</h4>
       <form action="add.php" class="white" method="POST">
       <label for="">Your Email</label>
-      <input type="text" name="email">
-      <label for="">Pizzat Title</label>
-      <input type="text" name="title">
-      <label for="">Ingredients Icomma separated:</label>
-      <input type="text" name="ingredients">
+      <input type="text" name="email" value="<?php echo $email ?>" >
+      <div class="red-text"><?php echo $errors['email'] ?></div>
+      <label for="">Pizza Title</label>
+      <input type="text" name="title" value="<?php echo $title ?>">
+      <div class="red-text"><?php echo $errors['title'] ?></div>
+      <label for="">Ingredients comma separated:</label>
+      <input type="text" name="ingredients" value="<?php echo $ingredients ?>">
+      <div class="red-text"><?php echo $errors['ingredients'] ?></div>
       <div class="center">
         <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
       </div>
